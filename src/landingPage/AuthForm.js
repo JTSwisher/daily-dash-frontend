d06d502eb99475e182e5e7bed45406c39a00e227
JTSwisher <jefftswisher@gmail.com>
2020-09-23 10:05:39 -0700

    add AuthForm component

---
+++ src/landingPage/AuthForm.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class AuthForm extends Component {
+
+    
+    
+    render() {
+        return (
+           <div>
+               
+           </div> 
+    
+        )
+    }
+    
+}
+
+export default AuthForm;
