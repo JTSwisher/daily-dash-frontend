c4c4c0570382dba075205bac025c0a97dddde0fb
JTSwisher <jefftswisher@gmail.com>
2020-09-23 13:50:29 -0700

    add custom buttons to Greeting component

--- src/landingPage/Greeting.js
+++ src/landingPage/Greeting.js
@@ -1,13 +1,16 @@
 import React from 'react';
-
-
-
+import { Link } from 'react-router-dom';
 
 function Greeting() {
     return (
         <div className="greeting">
             <h1>Welcome to DailyDash!</h1>
-            
+            <Link to="/authenticate">
+                <a className="greeting-button" target="_blank" rel="noopener noreferrer">Login</a>
+            </Link>
+            <Link to="/authenticate">
+                <a className="greeting-button" rel="noopener noreferrer">Signup</a>
+            </Link>
         </div>
     )
 }
