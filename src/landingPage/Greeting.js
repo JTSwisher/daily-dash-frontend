f8bf1ca8d991c1b1bb05b450763393630a959a60
JTSwisher <jefftswisher@gmail.com>
2020-09-23 13:27:57 -0700

    uninstall material ui

--- src/landingPage/Greeting.js
+++ src/landingPage/Greeting.js
@@ -1,18 +1,13 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
+
 
 
 function Greeting() {
     return (
         <div className="greeting">
             <h1>Welcome to DailyDash!</h1>
-            <Link to="/authenticate">
-                <Button inverted id="greeting-button">Login</Button>
-            </Link>
-            <Link to="/authenticate">
-                <Button inverted id="greeting-button">Signup</Button>
-            </Link>
+            
         </div>
     )
 }
