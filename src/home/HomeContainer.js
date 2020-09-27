530f5db33c691f0bb55f1203864d0ef6c1521fbd
JTSwisher <jefftswisher@gmail.com>
2020-09-27 12:35:40 -0700

    add getDate dunction to HomeContainer component for dash/home date rendering

--- src/home/HomeContainer.js
+++ src/home/HomeContainer.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Home.css'
 
+let getDate = () => {
+    let d = new Date().toString().split(' ');
+    let day = d.slice(0,1);
+    let month = d.slice(1,2);
+    let dayNumber = d.slice(2,3)
+    let currentDate = day + ", " + month + " " + dayNumber;
+    return currentDate
+}
+
 export default function HomeContainer() {
     return(
         <div className="home-container">
-            hi
+            <p className="home-greeting">Welcome Jeff</p>
+            <h4 className="home-date">{getDate()}</h4>
         </div>
     )
 }
