2a0d7d50739ae9043c23f2e1dc384f2f574a90d0
JTSwisher <jefftswisher@gmail.com>
2020-09-28 14:28:30 -0700

    add state to GoogleSearchBar function to control input value

--- src/home/GoogleSearchBar.js
+++ src/home/GoogleSearchBar.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { FaGoogle } from 'react-icons/fa'
 
 export default function GoogleSearchbar() {
 
+    const [query, setQuery] = useState('')
+
     return(
-        <form className="google-search-form" action="http://www.google.com/search" method="get" target="_blank" >
-            <input id="google-search-input" type="text" name="q" placeholder="Search Google.." />
+        <form className="google-search-form" action="http://www.google.com/search?q" method="get" target="_blank" onSubmit={() => setTimeout(() => setQuery(''), 1500)}>
+            <input id="google-search-input" type="text" placeholder="Search Google.." name="q" value={ query } onChange={(e) => setQuery(e.target.value)} />
             <button type="submit" id="google-button-wrapper"><FaGoogle id="google-search-icon"/></button>
         </form>
     )
