aec6be601b40171a233c9872aa535b6ed9155f80
JTSwisher <jefftswisher@gmail.com>
2020-09-30 19:58:54 -0700

    add icon to ShowsForm search bar

--- src/shows/ShowsForm.js
+++ src/shows/ShowsForm.js
@@ -1,10 +1,12 @@
 import  React, {useState} from 'react'
+import { FaSearch } from 'react-icons/fa'
 
 export default function ShowsForm() {
     
     return(
         <form className="shows-form">
             <input id="shows-input"></input>
+            <button type="submit" id="shows-button-wrapper"><FaSearch id="shows-form-submit-icon" /></button>
         </form>
     )
 
