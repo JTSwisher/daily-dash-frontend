63d0d527ca5d469dfe29d671b5c1b01f076f9d42
JTSwisher <jefftswisher@gmail.com>
2020-09-30 20:21:59 -0700

    add watch-now button & styling to Show card

--- src/shows/Show.js
+++ src/shows/Show.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 
+
 export default function Show(props){
     const [display, setDisplay] = useState(false)
     const {show} = props
@@ -8,15 +9,19 @@ export default function Show(props){
     let handleShowDetailDisplay = () => {
         setDisplay(!display);
         props.overlayCallback(!props.overlayState)
-       
     }
 
     return(
         <>
         <div id="show-details" onClick={() => handleShowDetailDisplay()} style={{display: (display ? '' : 'none')}}>
-            <img src={`https://image.tmdb.org/t/p/w200/${show.poster_path}`} id="show-details-image"/>
-            <p>Rating: {show.vote_average}/10</p>
-            <p>Release Date: {new Date(show.release_date).toLocaleDateString()}</p>
+            <div id="show-details-header-group">
+                <img src={`https://image.tmdb.org/t/p/w200/${show.poster_path}`} id="show-details-image"/>
+                <div id="header-details">
+                    <p id="show-details-rating">Rating: {show.vote_average}/10</p>
+                    <p id="show-details-date">Date: {new Date(show.release_date).toLocaleDateString()}</p>
+                    <button id="watch-show-button">Watch Now</button>
+                </div>
+            </div>
             <p id="show-details-description">{show.overview}</p>
         </div>
         <div className="show">
