8f3c9355c8beda5ddef34cab1c9031a11dbf2083
JTSwisher <jefftswisher@gmail.com>
2020-10-01 20:03:22 -0700

    add conditional rendering based on CategorySelected state for category styling. add categorySelected state to test fetch call to switch between movies and tv shows during fetch calls

--- src/shows/ShowsContainer.js
+++ src/shows/ShowsContainer.js
@@ -6,22 +6,33 @@ const API_KEY = process.env.REACT_APP_THE_MOVIE_DB_API_KEY
 
 export default function ShowsContainer() {
     const [shows, setShows] = useState([])
-    const [category, setCategory] = useState('movies')
+    const [categorySelected, setCategorySelected] = useState('movie')
     
     useEffect(() => { //temporary for testing content for rendering and styling
-        fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${API_KEY}&language=en-US&page=1`)
+       
+        fetch(`https://api.themoviedb.org/3/${categorySelected}/popular?api_key=${API_KEY}&language=en-US&page=1`)
         .then(res => res.json())
         .then(res => setShows(shows.concat(res["results"])))
-    }, [])
+    }, [categorySelected])
 
+    let categoryState = (e) => { 
+        setShows([])
+        setCategorySelected(e)
+    }
+
+    let movies = (categorySelected === 'movie') ? <p id="shows-header-title-1" onClick={ () => categoryState('movie')} style={{color: '#77A6F7'}}>Movies</p> 
+    : <p id="shows-header-title-1" onClick={ () => categoryState('movie')}>Movies</p>;
+
+    let tv = (categorySelected === 'tv') ? <p id="shows-header-title" onClick={ () => categoryState('tv')} style={{color: '#77A6F7'}}>TV</p> 
+    : <p id="shows-header-title" onClick={ () => categoryState('tv')}>TV</p>
 
 
     return(
         <div className="shows-page-container">
             <div className="shows-header">
                 <div className="shows-header-title">
-                    <p id="shows-header-title-1" value="movies">Movies</p>
-                    <p id="shows-header-title" value="tv">TV</p>
+                        { movies }
+                        { tv }
                 </div>
                 <div className="shows-header-form">
                     <ShowsForm />
