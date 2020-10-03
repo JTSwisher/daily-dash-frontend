c39ee07473d3236c8338cedffd8f8362c131c592
JTSwisher <jefftswisher@gmail.com>
2020-10-03 12:25:45 -0700

    add ternary expression to Show component to handle title rendering for tv vs movies

--- src/shows/Show.js
+++ src/shows/Show.js
@@ -4,13 +4,14 @@ import { FaTimes } from 'react-icons/fa'
 
 export default function Show(props){
     const [display, setDisplay] = useState(false)
-    const [streamingOptions, setStreamingOptions] = useState([])
     const {show} = props
    
     let handleShowDetailDisplay = () => {
         setDisplay(!display);
         props.overlayCallback(!props.overlayState)
     }
+
+    let name = show.title ? show.title : show.name;
 
     return(
         <>
@@ -19,11 +20,11 @@ export default function Show(props){
                 <img  src={`https://image.tmdb.org/t/p/w200/${show.poster_path}`} alt="" id="show-details-image"/>
                 <div id="header-details">
                     <FaTimes id="show-details-exit-icon" onClick={() => handleShowDetailDisplay()}/>
-                    <p id="show-details-title"><strong>Title:</strong> {show.title}</p>
+                    <p id="show-details-title"><strong>Title:</strong> {name}</p>
                     <p id="show-details-rating"><strong>Rating:</strong> {show.vote_average}/10</p>
                     <p id="show-details-date"><strong>Date:</strong> {new Date(show.release_date).toLocaleDateString()}</p>
                     <form action="http://www.google.com/search?q" method="get" target="_blank">
-                        <input type="hidden" name="q" value={show.title}/>
+                        <input type="hidden" name="q" value={name}/>
                         <button id="watch-show-button" type="submit">Watch Now</button>
                     </form>
                 </div>
@@ -31,7 +32,7 @@ export default function Show(props){
             <p id="show-details-description">{show.overview}</p>
         </div>
         <div className="show">
-            <p id="show-title">{show.title}</p>
+            <p id="show-title">{name}</p>
             <img src={`https://image.tmdb.org/t/p/w200/${show.poster_path}`} alt="" id="show-image" onClick={() => handleShowDetailDisplay()}/>
         </div>
         </>
