8ecbbd63b0e67d8d86d802d3ea76d9ef291649db
JTSwisher <jefftswisher@gmail.com>
2020-10-05 12:35:27 -0700

    add useEffect for setting fetch url dependant on query and category changes

--- src/shows/ShowsContainer.js
+++ src/shows/ShowsContainer.js
@@ -2,34 +2,53 @@ import React, {useState, useEffect} from 'react';
 import './Shows.css'
 import ShowsForm from './ShowsForm'
 import DisplayShows from './DisplayShows'
+
+import { popularMovies, popularTV, tvSearch, movieSearch } from './GetShows'
 const API_KEY = process.env.REACT_APP_THE_MOVIE_DB_API_KEY 
 
 export default function ShowsContainer() {
     const [shows, setShows] = useState([])
     const [categorySelected, setCategorySelected] = useState('movie')
+    const [url, setUrl] = useState('movie')
+    const [query, setQuery] = useState('')
 
-    // break out state into redux?
-    //need to handle category color change if search is used, pass calback down to form?
-    //need to break out all api calls into seperate file and impot in, use switch statement to determine which one to use? seperate calls one can 
-    // be shared for most popular like below passing type var. search needs seperate for either movie or tv search
     // use encodeURIComponent('string encode') to convert string into 'string%20'
 
-    //TV: https://api.themoviedb.org/3/search/tv?api_key=4668a91e8c6f6c418f6f0cd556b0e3d4&language=en-US&page=1&query=breaking%20bad&include_adult=false
-    //Movie: https://api.themoviedb.org/3/search/movie?api_key=4668a91e8c6f6c418f6f0cd556b0e3d4&language=en-US&query=ava&page=1&include_adult=false
-    //Popular: https://api.themoviedb.org/3/${categorySelected}/popular?api_key=${API_KEY}&language=en-US&page=1
-    
-    useEffect(() => { //temporary for testing content for rendering and styling
-        fetch(`https://api.themoviedb.org/3/${categorySelected}/popular?api_key=${API_KEY}&language=en-US&page=1`)
-        .then(res => res.json())
-        .then(res => setShows(shows.concat(res["results"])))
-    }, [categorySelected])
-
-    let categoryState = (e) => { 
-        setShows([])
-        setCategorySelected(e)
+    const type = {
+        "movie": popularMovies,
+        "tv": popularTV,
+        "tvSearch": tvSearch,
+        "movieSearch": movieSearch
     }
 
+    useEffect(() => {
+        if (categorySelected === 'movie' && query.length >= 1) setUrl('movieSearch');
+        if (categorySelected === 'tv' && query.length >= 1) setUrl('tvSearch');
+        if (categorySelected === 'movie' && query.length === 0) setUrl('movie');
+        if (categorySelected === 'tv' && query.length === 0) setUrl('tv')
+    }, [categorySelected, query])
+    
+    useEffect(() => { //temporary for testing content for rendering and styling
+        fetch(type[`${url}`](query))
+        .then(res => res.json())
+        .then(res => setShows(shows.concat(res["results"])))
+    }, [url, query])
 
+    let categoryState = (c) => { 
+        setShows([])
+        setQuery('')
+        setCategorySelected(c)
+        // setTimeout(() => {
+        //     setShows([])
+        //     setCategorySelected(c)
+        // }, 500)
+    }
+
+    let handleSearch = (c, q) => {
+        setShows([])
+        setUrl(c)
+        setQuery(q)
+    }
 
     return(
         <div className="shows-page-container">
@@ -39,10 +58,10 @@ export default function ShowsContainer() {
                     <p id="shows-header-title" onClick={ () => categoryState('tv')} style={{color: (categorySelected === 'tv' ? '#77A6F7' : 'white') }}>TV</p> 
                 </div>
                 <div className="shows-header-form">
-                    <ShowsForm />
+                    <ShowsForm category={categorySelected} search={handleSearch}/>
                 </div>
             </div>
-            <DisplayShows shows={shows}/>
+            <DisplayShows shows={shows} search={handleSearch}/>
         </div>
     )
 
--- src/shows/ShowsForm.js
+++ src/shows/ShowsForm.js
@@ -1,16 +1,22 @@
-import  React, {useState} from 'react'
+import  React, {useState, useEffect} from 'react'
 import { FaSearch } from 'react-icons/fa'
 
-export default function ShowsForm() {
+export default function ShowsForm(props) {
     const [query, setQuery] = useState('')
+    const [type, setType] = useState('')
+
+    useEffect(() => {
+        props.category === 'movie' ? setType("movieSearch") : setType("tvSearch")
+    }, [props.category])
     
-    let handleSubmit = e => {
+    let handleFormSubmit = e => {
         e.preventDefault()
+        props.search(type, query)
         setQuery('')
     }
 
     return(
-        <form className="shows-form" onSubmit={e => handleSubmit(e)}>
+        <form className="shows-form" onSubmit={e => handleFormSubmit(e)}>
             <input id="shows-input" value={query} onChange={e => setQuery(e.target.value)}></input>
             <button type="submit" id="shows-button-wrapper"><FaSearch id="shows-form-submit-icon" /></button>
         </form>
