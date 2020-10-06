e4c29459d2fee802b6c6e89baae75ea89d4c37ac
JTSwisher <jefftswisher@gmail.com>
2020-10-05 19:06:04 -0700

    add form & input state to WeatherForm component, use WeatherContainer callback to set location state in form parent component

--- src/weather/WeatherContainer.js
+++ src/weather/WeatherContainer.js
@@ -1,10 +1,16 @@
 import React, {useState, useEffect} from 'react'
+import WeatherForm from './WeatherForm'
 
 export default function WeatherContainer() {
+    const [location, setLocation] = useState(false)
+
+    let handleLocationFormSubmit = (z) => {
+        setLocation(z)
+    }
 
     return(
         <div className="weather-container">
-
+            <WeatherForm location={location} updateLocationState={handleLocationFormSubmit}/>
         </div>
     )
 }
---
+++ src/weather/WeatherForm.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+import { FaSearch } from 'react-icons/fa'
+
+export default function WeatherForm(props) {
+    const [zipcode, setZipcode] = useState('')
+
+    let handleFormSubmit = (e) => {
+        e.preventDefault();
+        props.updateLocationState(zipcode)
+    }
+
+    return(
+        <div className="weather-form" style={{display: (props.location ? 'none' : '')}}>
+            <form id="weather-form" onSubmit={(e) => handleFormSubmit(e)}>
+                <input id="weather-input" value={zipcode} onChange={(e) => setZipcode(e.target.value)} placeholder="Zipcode..." required></input>
+                <button type="submit" id="shows-button-wrapper"><FaSearch id="shows-form-submit-icon" /></button>
+            </form>
+        </div>
+    )
+}
