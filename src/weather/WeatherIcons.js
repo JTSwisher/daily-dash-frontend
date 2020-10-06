6480b31b6df08b2dea9376eb4f6841d5dba87ad0
JTSwisher <jefftswisher@gmail.com>
2020-10-06 16:39:03 -0700

    create WeatherIcons object to hold icons used for weather rendering

---
+++ src/weather/WeatherIcons.js
@@ -0,0 +1,8 @@
+import { WiDaySunny, WiCloudy, WiDayThunderstorm } from 'react-icons/wi'
+
+export const weatherIcons = {
+    sunny: WiDaySunny, // 1-5
+    cloudy: WiCloudy, // 6-11
+    raining: WiDayThunderstorm, //12-18
+    snow: WiDaySnow, //19-29
+}
