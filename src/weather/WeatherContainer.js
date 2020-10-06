9823c161d54ddef5127be2dad1a642e1c52faafd
JTSwisher <jefftswisher@gmail.com>
2020-10-05 19:35:38 -0700

    add weather api key to .env file import into WeatherContainer

--- src/weather/WeatherContainer.js
+++ src/weather/WeatherContainer.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react'
 import './Weather.css'
 import WeatherForm from './WeatherForm'
 
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY  
+
 export default function WeatherContainer() {
     const [isLocationGiven, setIsLocationGiven] = useState(false)
 
@@ -9,7 +11,7 @@ export default function WeatherContainer() {
         setIsLocationGiven(!isLocationGiven)
     }
 
-    
+
 
     return(
         <div className="weather-container">
