08f41b318c910e77c039553765ec0c8bd3ba7522
JTSwisher <jefftswisher@gmail.com>
2020-10-08 18:21:16 -0700

    importchange signup component to set localstorage items for user name and id

--- src/authentication/Signup.js
+++ src/authentication/Signup.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import AuthForm from './AuthForm'
+import history from '../History'
 import './Auth.css'
 
 const submitUser = user => { //temp will be handled by redux dispatch
@@ -12,7 +13,20 @@ const submitUser = user => { //temp will be handled by redux dispatch
         body: JSON.stringify({user: user})
     })
     .then(res => res.json())
-    .then(user => console.log(user))
+    .then(user =>{
+        if (user.error) {
+            window.alert(user.error)
+        } else {
+            console.log(user)
+            localStorage.setItem('userId', user.user.id )
+            localStorage.setItem('name', user.user.name)
+            history.push("/dash/home")
+        }
+    })
+    .catch((error) => {
+        console.log(error)
+        localStorage.clear()
+    })
 }
 
 export default function Signup() {
