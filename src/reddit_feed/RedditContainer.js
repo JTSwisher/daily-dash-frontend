5e7a8394553ba8ca6071efc2f39b97e1ce9e65c8
JTSwisher <jefftswisher@gmail.com>
2020-10-10 12:20:11 -0700

    add useEffect hook to RedditContainer to fetch data based on subredditValue

--- src/reddit_feed/RedditContainer.js
+++ src/reddit_feed/RedditContainer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Reddit.css'
 import RedditForm from './RedditForm'
 import { FiSearch } from 'react-icons/fi'
@@ -6,11 +6,23 @@ import { FiSearch } from 'react-icons/fi'
 export default function RedditContainer() {
     const [subredditValue, setSubredditValue] = useState('webdev')
     const [isFormDisplay, setIsFormDisplay] = useState(false)
-
+    const [subRedditResults, setSubRedditResults] = useState([])
 
     const searchReddit = (value) => {
-
+        setSubredditValue(value)
     }
+
+    useEffect(() => {
+        fetch(`https://www.reddit.com/r/${subredditValue}.json`)
+        .then(res => res.json())
+        .then(res => {
+            setSubRedditResults(res.data.children)
+        })
+
+    }, [subredditValue])
+
+    //create useEffect to fetch reddit data
+    //pass data into display data component which passes into data compoennet
 
     let redditForm;
     if (isFormDisplay) redditForm = <RedditForm search={searchReddit} updateFormDisplay={setIsFormDisplay}/>;
