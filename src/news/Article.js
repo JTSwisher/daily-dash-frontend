fe892958b501e7c709b3dd566b543bd13991d092
JTSwisher <jefftswisher@gmail.com>
2020-10-11 13:02:08 -0700

    render article data, add article styling

--- src/news/Article.js
+++ src/news/Article.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 export default function Article(props) {
+
+
     return (
         <div className="article">
-            
+            <div id="image-container">
+                <a href={props.article.url} target="_blank" rel="noopener noreferrer"><img className="article-image" src={props.article.urlToImage } /></a>
+            </div>
+            <p>{props.article.title.slice(0, 48)}...</p>
         </div>
     )
 }
