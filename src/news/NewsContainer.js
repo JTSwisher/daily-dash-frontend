4b0efe79e3feaddf57d502d5c3b936d5d5a2ca42
JTSwisher <jefftswisher@gmail.com>
2020-10-10 19:40:04 -0700

    import NavigationBar into NewsContainer

--- src/news/NewsContainer.js
+++ src/news/NewsContainer.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import NavigationBar from '../NavigationBar'
 
 export default function NewsContainer() {
     return (
-        <div>
-            
-        </div>
+        <>
+        <NavigationBar />
+        </>
     )
 }
