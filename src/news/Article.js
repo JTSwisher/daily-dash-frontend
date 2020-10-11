bdc734677743102f009070f0f3ddef79afc0b740
JTSwisher <jefftswisher@gmail.com>
2020-10-11 13:16:34 -0700

    add conditional rendering of article image if resuly is null

--- src/news/Article.js
+++ src/news/Article.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import { MdBrokenImage } from 'react-icons/md'
 
 export default function Article(props) {
 
-
+    let image = props.article.urlToImage === null ? <MdBrokenImage id="no-image-icon" /> : <img className="article-image" src={props.article.urlToImage } />
+    
     return (
         <div className="article">
             <div id="image-container">
-                <a href={props.article.url} target="_blank" rel="noopener noreferrer"><img className="article-image" src={props.article.urlToImage } /></a>
+                <a href={props.article.url} target="_blank" rel="noopener noreferrer">{ image }</a>
             </div>
-            <p>{props.article.title.slice(0, 48)}...</p>
+            <a id="article-title" href={props.article.url} target="_blank" rel="noopener noreferrer">{props.article.title.slice(0, 48)}...</a>
         </div>
     )
 }
