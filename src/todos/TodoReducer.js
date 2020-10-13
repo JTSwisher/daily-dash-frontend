bc4ce63a4f5313edf96c8956c8e281e4c2086112
JTSwisher <jefftswisher@gmail.com>
2020-10-13 16:51:15 -0700

    add todo delete case statements in TodoReducer

--- src/todos/TodoReducer.js
+++ src/todos/TodoReducer.js
@@ -8,6 +8,11 @@ const todoReducer = (state= {todos: [], requesting: false}, action) => {
             return {...state, todos: [], requesting: true}
         case "TODOS_RECEIVED":
             return {...state, todos: [...state.todos].concat(action.todos), requesting: false}
+        case "DELETING_TODO":
+            return {...state, todos: [...state.todos], requesting: true}
+        case "TODO_DELETED": //filter out todo based on id, return remaining todos
+            const todos = state.todos.filter(todo => todo.id !== action.id);
+            return { todos , requesting: false }
         default:
             return state;
     }
